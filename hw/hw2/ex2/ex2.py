d21318f93a136571e0b139d9e33f9dc08a687608
Liu Chang <lllccc2009@gmail.com>
2015-12-04 21:11:34 -0500

    fix error in unigram and parsing of bigram models
    works OK for unigram:
    1) probability calculation
    2) sum of the count(total, not just word set count)
    3) fix some minor bugs

--- hw/hw2/ex2/ex2.py
+++ hw/hw2/ex2/ex2.py
@@ -8,7 +8,7 @@ import collections
 import math
 
 DEBUG = True
-TEST = True
+TEST = False
 
 def dbgprint(s):
     if DEBUG:
@@ -21,6 +21,7 @@ def unigrams(text):
         uni.append([token])
     return uni
 
+# the input is the text that contains all the tokens(using List)
 def bigrams(text):
     bi = []
     token_addr = 0
@@ -29,6 +30,7 @@ def bigrams(text):
         token_addr += 1
     return bi
 
+# the input is the text that contains all the tokens(using List)
 def trigrams(text):
     tri = []
     token_addr = 0
@@ -37,6 +39,7 @@ def trigrams(text):
         token_addr += 1
     return tri
 
+# the input is the text that contains all the tokens(using List)
 def ngram(text, count_n):
     model = []
     count = 0
@@ -52,9 +55,10 @@ def load_text(file):
     file_handler = open(file, "r+")
     for file in file_handler:
         lists = file[:-2].split(' ') # eliminate the \r\n at the end
-        print lists
-        print file
-        tokens = tokens + lists
+        list_new = [word.lower() for word in lists]
+        #print lists
+        #print file
+        tokens = tokens + list_new
     return tokens
 
 ## Test the result for x-gram algorithms
@@ -65,14 +69,16 @@ def test_gram():
     triresult = trigrams(result)
     fourresult = ngram(result, 4)
 
-    # TEST n-gram result
-    dbgprint(result)
-    dbgprint(uniresult)
-    dbgprint(biresult)
-    dbgprint(triresult)
-    dbgprint(fourresult)
+    # TEST n-gram result(output ALL grams, but not dicts, as repeat items)
+    #dbgprint(result)
+    #dbgprint(uniresult)
+    #dbgprint(biresult)
+    #dbgprint(triresult)
+    #dbgprint(fourresult)
 
     get_grams_dict(uniresult)
+    get_grams_dict(biresult)
+    get_grams_dict(triresult)
 
 ## Test saving models
 def test_save_models():
@@ -97,13 +103,15 @@ def get_grams_dict(listitems):
             seen_account.append(1)
         else:
             index = seen.index(x)
-            dbgprint("index = %s"%index)
+            #dbgprint("index = %s"%index)
             seen_account[index] += 1
 
-    print "raw ", listitems, len(listitems)
-    print "seen ", seen, len(seen)
-    print "uniq ", uniq, len(uniq)
-    print "seen_account ", seen_account, len(seen_account)
+    # for DEBUG:
+    #print "raw ", listitems, len(listitems)
+    #print "seen ", seen, len(seen)
+    #print "uniq ", uniq, len(uniq)
+    #print "seen_account ", seen_account, len(seen_account)
+    #print seen, seen_account
 
     return seen, seen_account
 
@@ -144,6 +152,7 @@ def save_training_model(n, train_file_name, model_name):
     model.close()
 
 
+# for unigram model, parse is different from other models
 def load_model(train_model_name):
     grams = []
     counts = []
@@ -152,71 +161,104 @@ def load_model(train_model_name):
 
     for item in handler:
         items = item.split('\t')
-        print items
+        #print items
         # eliminate the " " of the string after splitting
-        print "*** items ***", items[0], items[1]
+        #print "*** items ***", items[0], items[1]
         grams.append([items[0][2:-2]])
         # account ends with "\r\n" when writing
         counts.append(int(items[1][:-2]))
 
-    print "load_model: gram and counts = ", grams, counts
+    #print "load_model: gram and counts = ", grams, counts
+    return grams, counts
+
+# 2-gram model is different from unigram since it has multiple words
+# here it's quite complex to parse the model, as even though we treat it
+# as list[], when parsing it regards as String, so I need to parse each word
+# and then construct a new Insert list to append
+def load_2gram_model(train_model_name):
+    grams = []
+    counts = []
+    insert = []
+
+    handler = open(train_model_name, "r+")
+
+    for item in handler:
+        items = item.split('\t')
+        # item[0] looks like list[], but it's string after parsing, reform it
+        #print "#### items[0]", items[0]
+        #print "#### items[1]", items[1][:-2]
+        #print "#### splits", items[0][2:-2].split("'")
+        tmp = items[0][2:-2].split("'")
+        insert.append(tmp[0])
+        insert.append(tmp[-1])
+
+        grams.append(insert)
+        counts.append(int(items[1][:-2]))
+        insert = []
+
+    #print "***********", grams
     return grams, counts
 
 
 ## Get unigram probability, Input is the model, test data, and delta
+## For OOV(Out of Vacabulary), I set it as the 1.0/sum_count
 def get_unigram_prob(train_model_name, test_file_name, delta ):
     total_proba = 0
     file_handler = open(test_file_name, "r+")
     model_gram, model_count = load_model(train_model_name)
 
     probability = 0
-    total_count = len(model_gram)
-    print "total_count", total_count
+    total_count = sum(model_count)
+    #print "total_count", total_count
 
     # get each sentence and calculate the probability
     for line in file_handler:
         line_set = line[:-1].split(' ')
-        print "linesets: ", line_set
-        gram = unigrams(line_set)
-        print "line = ", line
+        #print "linesets: ", line_set
+        line_set_new = [word.lower() for word in line_set]
+        gram = unigrams(line_set_new)
+        #print "line = ", line
+        #print "gram = ", gram
 
         line_probability = 1
         for uniset in gram:
-            print uniset
+            #print uniset
             # item in the model, then get its count and probability
             if uniset in model_gram:
                index = model_gram.index(uniset)
                this_item_total_count = model_count[index]
-               print "this_item_total_count", this_item_total_count
+               #print "this_item_total_count", this_item_total_count
                this_probability = (this_item_total_count + delta)/(total_count + delta)
-               print this_probability
-            # not in this list, using add-smothing it's 1
+               #print this_probability
+            # not in this list(OOV, out of vacabulary), using add-smothing it's 1
             else:
                 this_count = 0
-                this_probability = 1
+                this_probability = 1.0/total_count
             line_probability *= this_probability
-        print "line_probability = ", line_probability
+        #print "line_probability = ", line_probability
 
-        probability += line_probability
+        probability += math.log(line_probability, 10)
 
-    print 'unigram probability = ', probability
-    return math.log(probability, 10)
+    #print 'unigram probability = ', probability
+    return probability
 
 ## Get bigram probability, Input is the model, test data and delta for smoothing
 def get_bigram_prob(train_model_name, test_file_name, delta ):
     total_proba = 0
     file_handler = open(test_file_name, "r+")
-    model_gram, model_count = load_model(train_model_name)
+    model_gram, model_count = load_2gram_model(train_model_name)
 
     probability = 0
-    total_count = len(model_gram)
+    total_count = sum(model_count)
     print "total_count", total_count
+    print "model_gram", model_gram
 
     # get each sentence and calculate the probability
     for line in file_handler:
         line_set = line[:-1].split(' ')
-        print "linesets: ", line_set
-        gram = bigrams(line_set)
+        line_set_new = [word.lower() for word in line_set]
+        print "linesets: ", line_set_new
+        gram = bigrams(line_set_new)
         print "line = ", line
 
         line_probability = 1
@@ -226,6 +268,7 @@ def get_bigram_prob(train_model_name, test_file_name, delta ):
             if biset in model_gram:
                index = model_gram.index(biset)
                exact_count = model_count[index]
+               print "###index=%d, count=%d"%(index, exact_count)
 
                # for conditional probability, calculate the parital count
                this_item_total_count = 0
@@ -239,59 +282,15 @@ def get_bigram_prob(train_model_name, test_file_name, delta ):
             # not in this list, using add-smothing it's 1
             else:
                 this_count = 0
-                this_probability = 1
+                this_probability = 1.0/total_count
             line_probability *= this_probability
         print "line_probability = ", line_probability
 
-        probability += line_probability
-
-    print 'unigram probability = ', probability
-    return math.log(probability, 10)
-
-
-## Get bigram probability, Input is the whole text
-def get_bigram_prob(train_model_name, test_file_name, delta ):
-    total_proba = 0
-    file_handler = open(test_file_name, "r+")
-    model_gram, model_count = load_model(train_model_name)
-
-    probability = 0
-
-    # get each sentence and calculate the probability
-    for line in file_handler:
-        line_set = line[:-1].split(' ')
-        print "linesets: ", line_set
-        gram = bigrams(line_set)
-        print "line = ", line
-
-        line_probability = 1
-        for biset in gram:
-            print biset
-            # item in the model, then get its count and probability
-            if biset in model_gram:
-               index = model_gram.index(biset)
-               exact_count = model_count[index]
-
-               # for conditional probability, calculate the parital count
-               this_item_total_count = 0
-               for i in model_gram:
-                   if i[0] == biset[0]:
-                       this_item_total_count += 1
-
-               print "this_item_total_count", this_item_total_count
-               this_probability = (this_item_total_count + delta)/(exact_count + delta)
-               print this_probability
-            # not in this list, using add-smothing it's 1
-            else:
-                this_count = 0
-                this_probability = 1
-            line_probability *= this_probability
-        print "line_probability = ", line_probability
-
-        probability += line_probability
+        probability += math.log(line_probability, 10)
 
     print 'bigram probability = ', probability
-    return math.log(probability, 10)
+    return probability
+
 
 ## Get trigram probability, Input is the whole text
 def get_trigram_prob(train_model_name, test_file_name, delta ):
@@ -300,11 +299,14 @@ def get_trigram_prob(train_model_name, test_file_name, delta ):
     model_gram, model_count = load_model(train_model_name)
 
     probability = 0
+    total_count = sum(model_count)
+    print "total_count", total_count
 
     # get each sentence and calculate the probability
     for line in file_handler:
         line_set = line[:-1].split(' ')
-        print "linesets: ", line_set
+        line_set_new = [word.lower() for word in line_set]
+        print "linesets: ", line_set_new
         gram = trigrams(line_set)
         print "line = ", line
 
@@ -344,6 +346,12 @@ def train():
 def test():
     return 1
 
+## for debugging, just test some function
+def debug():
+    if TEST == True:
+        test_gram()
+        test_save_models()
+
 def parse_parameter():
 
     return 1
@@ -358,10 +366,10 @@ if __name__ == "__main__":
         test_save_models()
 
     # now use the model to do the testing
-    uni_likelihood = get_unigram_prob("output_1_gram.model", "sample-test-data.txt", 0.0001)
-    bi_likelihood = get_bigram_prob("output_2_gram.model", "sample-test-data.txt", 0.0001)
-    tri_likelihood = get_trigram_prob("output_3_gram.model", "sample-test-data.txt", 0.0001)
+    uni_likelihood = get_unigram_prob("output_1_gram.model", "sample-test-data.txt", 0.0)
+    bi_likelihood = get_bigram_prob("output_2_gram.model", "sample-test-data.txt", 0.0)
+    #tri_likelihood = get_trigram_prob("output_3_gram.model", "sample-test-data.txt", 0.0001)
 
     print "uni_likelihood = ", uni_likelihood
     print "bi_likelihood =", bi_likelihood
-    print "tri_likelihoo =", tri_likelihood
+    #print "tri_likelihoo =", tri_likelihood
